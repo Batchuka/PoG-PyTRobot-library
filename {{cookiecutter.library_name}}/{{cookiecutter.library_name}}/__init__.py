7659c02c0dd728a778382ce206052c12528d8d38
Matheus C. Delfino <matheus.corteletti@wmtrading.com.br>
2023-08-10 15:03:11 -0300

    estruturação 5

--- {{cookiecutter.library_name}}/{{cookiecutter.library_name}}/__init__.py
+++ {{cookiecutter.library_name}}/{{cookiecutter.library_name}}/__init__.py
@@ -1,6 +1,6 @@
 # minha_biblioteca/__init__.py
 
 # Importar as funções e classes dos módulos
-from .package_config import {{cookiecutter.class_name}}
+from .{{cookiecutter.library_name}} import {{cookiecutter.class_name}}
 
 # Importar arquivos subsequentes
